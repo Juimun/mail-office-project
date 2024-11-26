8b34a11d2f45faf8398981636e03e62d01e4c24e
Juimun <2002kofanov@gmail.com>
2024-11-26 20:55:41 +0300

    Feature: Added new menu for Staff

---
+++ MailOffice/View/SpecialMenuWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using MailOffice.ViewModel;
+
+namespace MailOffice.View;
+
+/// <summary>
+/// Логика взаимодействия для SpecialMenuWindow.xaml
+/// </summary>
+public partial class SpecialMenuWindow : Window {
+
+    private SolidColorBrush _originalBackgroundBrush;
+    private SolidColorBrush _originalForegroundBrush;
+
+    public SpecialMenuWindow() {
+        InitializeComponent();
+
+        DataContext = new SpecialMenuWindowViewModel(this);
+    }
+
+    private void UIElement_OnMouseEnter(object sender, MouseEventArgs mouseEventArgs)
+    {
+        Button button = (Button)sender;
+        _originalBackgroundBrush = (SolidColorBrush)button.Background;
+        _originalForegroundBrush = (SolidColorBrush)button.Foreground;
+
+        button.Foreground = Brushes.Black;
+    }
+
+
+    private void UIElement_OnMouseLeave(object sender, MouseEventArgs mouseEventArgs)
+    {
+        Button button = (Button)sender;
+
+        // Восстанавливаем исходные цвета
+        button.Background = _originalBackgroundBrush;
+        button.Foreground = _originalForegroundBrush;
+    }
+}
+
---
+++ MailOffice/ViewModel/SpecialMenuWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MailOffice.Infrastructure;
+using MailOffice.View;
+
+namespace MailOffice.ViewModel;
+
+public class SpecialMenuWindowViewModel(SpecialMenuWindow hostWindow) {
+
+    private SpecialMenuWindow HostWindow { get; set; } = hostWindow;
+
+    public RelayCommand ExitCommand => new(
+       obj => HostWindow.Close(),
+       obj => true
+   );
+
+} //SpecialMenuWindowViewModel
+
