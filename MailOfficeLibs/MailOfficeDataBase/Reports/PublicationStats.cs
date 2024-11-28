bd5d90af9939f77f1d9805e1dcbc7ece2469b073
Juimun <2002kofanov@gmail.com>
2024-11-28 12:07:36 +0300

    Remove: Unused records

--- MailOfficeLibs/MailOfficeDataBase/Reports/PublicationStats.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MailOfficeDataBase.Reports;
-
-// По каждому изданию указывается средний срок подписки и количество экземпляров
-public record PublicationStats(
-    int SubscriptionAvg,
-    int Quantity
-    );
-    
-
