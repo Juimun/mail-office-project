c2f2c876ab4e916fa41cd2cc8ae8a469b94c02af
Juimun <2002kofanov@gmail.com>
2024-11-30 20:51:33 +0300

    Seed: Update full name generator

--- MailOfficeLibs/MailOfficeFactory/Factories/FactoryPerson.cs
+++ MailOfficeLibs/MailOfficeFactory/Factories/FactoryPerson.cs
@@ -3,21 +3,22 @@ using MailOfficeEntities.Entities.Accounts;
 using MailOfficeTool.Infrastructure;
 
 
-namespace MailOfficeFactory.Factories; 
+namespace MailOfficeFactory.Factories;
 
 public static partial class Factory {
 
     // Создание случайной сущности Person 
-    public static Person GetPerson(int userId) => new()
-        {
-            FirstName = GetRandomFirstName(),
-            SecondName = GetRandomSurname(),
-            Patronymic = GetRandomPatronymic(),
+    public static Person GetPerson(int userId) {
+        var gender = Utils.GetRandom(0, 1);
+        return new() {
+            FirstName = GetRandomFirstName(gender),
+            SecondName = GetRandomSurname(gender),
+            Patronymic = GetRandomPatronymic(gender),
             Role = GetRandomRole(),
             PreviousRole = PersonCategory.Registered,
             UserId = userId,
-        }; 
-   
+        };
+    } 
 
     // Создание списка сущностей Person 
     public static List<Person> GetPeople(List<User> users, Func<int, Person> getPerson) => users
@@ -27,45 +28,53 @@ public static partial class Factory {
     // Генератор случайной роли PersonCategory
     private static int _countStaff = 0, _countSubscriber = 0;
     private static PersonCategory GetRandomRole() {
-       while (true) {
-           switch (Utils.GetRandom(1, 3)) {
-               case 1:
-                   if (_countStaff < 50) {
+        while (true) {
+            switch (Utils.GetRandom(1, 3)) {
+                case 1:
+                    if (_countStaff < 50) {
                         _countStaff++;
-                       return PersonCategory.Staff;
-                   }
-                   continue;
-               case 2:
-                   if (_countSubscriber < 100) {
+                        return PersonCategory.Staff;
+                    }
+                    continue;
+                case 2:
+                    if (_countSubscriber < 100) {
                         _countSubscriber++;
-                       return PersonCategory.Subscriber;
-                   }
-                   continue;
+                        return PersonCategory.Subscriber;
+                    }
+                    continue;
                 default:
-                   return PersonCategory.Registered;
-           } //switch
-       } //while
+                    return PersonCategory.Registered;
+            } //switch
+        } //while
     }
 
-// Выбор "случайного" отчества из тестового массива
-private static string GetRandomPatronymic() => 
-        Patronymics[Utils.GetRandom(0, Patronymics.Length - 1)];
+    // Выбор "случайного" отчества из тестового массива
+    private static string GetRandomPatronymic(int gender) => gender == 1 ?
+            Patronymics[Utils.GetRandom(0, Patronymics.Length - 1)] + "ич" : Patronymics[Utils.GetRandom(0, Patronymics.Length - 1)] + "на";
 
     // Выбор "случайной" фамилии из тестового массива
-    private static string GetRandomSurname() =>
-        Surnames[Utils.GetRandom(0, Surnames.Length - 1)];
+    private static string GetRandomSurname(int gender) => gender == 1 ?
+        Surnames[Utils.GetRandom(0, Surnames.Length - 1)] : Surnames[Utils.GetRandom(0, Surnames.Length - 1)] + "а";
 
     // Выбор "случайного" имени из тестового массива
-    private static string GetRandomFirstName() =>
-        Names[Utils.GetRandom(0, Names.Length - 1)];
+    private static string GetRandomFirstName(int gender) => gender == 1 ?
+        MaleNames[Utils.GetRandom(0, MaleNames.Length - 1)] : FemaleNames[Utils.GetRandom(0, FemaleNames.Length - 1)];
 
     #region ФИО для тестов 
-    private static readonly string[] Names = [
-        "Алина", "Артем", "Валерия", "Виктор", "Даниил", "Дарья", "Егор", "Екатерина", "Иван", "Ирина",
-        "Кирилл", "Кристина", "Максим", "Мария", "Николай", "Наталья", "Олег", "Ольга", "Павел", "Полина",
-        "Роман", "Светлана", "Сергей", "София", "Тимофей", "Юлия", "Александр", "Анастасия", "Андрей", "Анна",
-        "Борис", "Вера", "Дмитрий", "Евгения", "Георгий", "Елена", "Игорь", "Ксения", "Леонид", "Любовь",
-        "Михаил", "Марина", "Петр", "Татьяна", "Руслан", "Елизавета", "Алексей", "Вадим", "Глеб", "Евгения"
+    private static readonly string[] MaleNames = [
+        "Александр", "Алексей", "Анатолий", "Андрей", "Антон", "Аркадий", "Артем", "Артур", "Борис", "Вадим",
+        "Валентин", "Валерий", "Владлен", "Владимир", "Владислав", "Вячеслав", "Георгий", "Геннадий", "Герман",
+        "Григорий", "Даниил", "Денис", "Дмитрий", "Евгений", "Егор", "Захар", "Иван", "Игорь", "Илья", "Кирилл",
+        "Константин", "Леонид", "Максим", "Михаил", "Никита", "Николай", "Олег", "Павел", "Петр", "Роман",
+        "Руслан", "Сергей", "Семен", "Станислав", "Степан", "Тимофей", "Федор", "Юрий", "Яков", "Эдуард"
+    ];
+
+    private static readonly string[] FemaleNames = [
+        "Агата", "Александра", "Алина", "Алла", "Анастасия", "Ангелина", "Анна", "Антонина", "Валентина", "Валерия",
+        "Варвара", "Вера", "Виктория", "Виолетта", "Галина", "Дарья", "Евгения", "Елена", "Елизавета", "Жанна",
+        "Зинаида", "Ирина", "Карина", "Катерина", "Клавдия", "Ксения", "Лариса", "Лидия", "Любовь", "Людмила",
+        "Маргарита", "Мария", "Марина", "Марта", "Наталья", "Нина", "Оксана", "Ольга", "Полина", "София",
+        "Светлана", "Татьяна", "Юлия", "Яна", "Жанна", "Людмила", "Алёна", "Ирина", "Анжела", "Диана"
     ];
 
     private static readonly string[] Surnames = [
@@ -77,14 +86,13 @@ private static string GetRandomPatronymic() =>
     ];
 
     private static readonly string[] Patronymics = [
-        "Александрович", "Алексеевич", "Андреевич", "Артемович", "Борисович",
-        "Вадимович", "Викторович", "Владимирович", "Дмитриевич", "Евгеньевич",
-        "Игоревич", "Ильич", "Константинович", "Максимович", "Николаевич",
-        "Олегович", "Павлович", "Петрович", "Сергеевич", "Тимофеевич",
-        "Александровна", "Алексеевна", "Андреевна", "Артемовна", "Борисовна",
-        "Вадимовна", "Викторовна", "Владимировна", "Дмитриевна", "Евгеньевна",
-        "Игоревна", "Ильинична", "Константиновна", "Максимовна", "Николаевна",
-        "Олеговна", "Павловна", "Петровна", "Сергеевна", "Тимофеевна"
+        "Александров", "Алексеев", "Анатольев", "Андреев", "Антонов", "Аркадьев", "Артемов", "Артуров",
+        "Борисов", "Вадимов", "Валентинов", "Валерьев", "Владимиров", "Владиславов", "Вячеславов",
+        "Георгиев", "Геннадьев", "Германов", "Григорьев", "Данилов", "Денисов", "Дмитриев",
+        "Евгеньев", "Егоров", "Захаров", "Иванов", "Игорьев", "Ильин", "Кириллов", "Константинов",
+        "Леонидов", "Максимов", "Михайлов", "Никитин", "Николаев", "Олегов", "Павлов", "Петров",
+        "Романов", "Русланов", "Сергеев", "Семенов", "Станиславов", "Степанов", "Тимофеев",
+        "Федоров", "Юрьев", "Яковлев", "Эдуардов"
     ];
     #endregion
 } //FactoryPerson
